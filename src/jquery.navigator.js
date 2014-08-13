a306cf905f29f7b80dbf2dc93df7219784a5963b
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-08-13 01:52:57 -0400

    All methods (←↑→↓) implemented with unit tests.

--- src/jquery.navigator.js
+++ src/jquery.navigator.js
@@ -25,10 +25,10 @@
   //-------------//
 
   var Navigator = function(element, options) {
-    this.document = $(document);
-    this.element = element;
-    this.options = $.extend({}, Navigator.defaults, options);
-    this.selected = null;
+    this.$document = $(document);
+    this.$element = element;
+    this.$options = $.extend({}, Navigator.defaults, options);
+    this.$selected = null;
     this.enable();
   };
 
@@ -47,14 +47,14 @@
   Navigator.prototype.enable = function() {
     // Create map of movement methods by keys.
     var keys = {};
-    keys[this.options.left] = this.left;
-    keys[this.options.up] = this.up;
-    keys[this.options.right] = this.right;
-    keys[this.options.down] = this.down;
+    keys[this.$options.left] = this.left;
+    keys[this.$options.up] = this.up;
+    keys[this.$options.right] = this.right;
+    keys[this.$options.down] = this.down;
 
     // Bind keydown event.
     var instance = this;
-    this.document.bind('keydown', function(event) {
+    this.$document.bind('keydown', function(event) {
       if (keys[event.which]) {
         keys[event.which].call(instance);
       }
@@ -62,40 +62,147 @@
   };
 
   Navigator.prototype.disable = function() {
-    this.document.unbind('keydown');
+    this.$document.unbind('keydown');
   };
 
   Navigator.prototype.destroy = function() {
     this.disable();
-    this.element.removeData('navigator');
+    this.$element.removeData('navigator');
   };
 
   Navigator.prototype.left = function() {
-    if (!this.selected) {
-      this.element.children().eq(0).addClass(this.options.selected);
+    if (!this.$selected) {
+      this.select(this.$element.children().eq(0));
+    } else {
+      var left = this.$selected.position().left - 1;
+      var top = this.$selected.position().top;
+
+      var next = this.elementsBefore(left, Infinity).reduce(function(prev, curr) {
+        curr = $(curr);
+        var currDistance = curr.position();
+        currDistance = Math.abs(left - currDistance.left) + Math.abs(top - currDistance.top);
+        if (currDistance < prev.distance) {
+          return {
+            distance: currDistance,
+            element: curr
+          };
+        }
+        return prev;
+      }, {
+        distance: Infinity
+      });
+
+      this.select(next.element);
     }
   };
 
   Navigator.prototype.up = function() {
-    if (!this.selected) {
-      this.element.children().eq(0).addClass(this.options.selected);
+    if (!this.$selected) {
+      this.select(this.$element.children().eq(0));
+    } else {
+      var left = this.$selected.position().left;
+      var top = this.$selected.position().top - 1;
+
+      var next = this.elementsBefore(Infinity, top).reduce(function(prev, curr) {
+        curr = $(curr);
+        var currDistance = curr.position();
+        currDistance = Math.abs(left - currDistance.left) + Math.abs(top - currDistance.top);
+        if (currDistance < prev.distance) {
+          return {
+            distance: currDistance,
+            element: curr
+          };
+        }
+        return prev;
+      }, {
+        distance: Infinity
+      });
+
+      this.select(next.element);
     }
   };
 
   Navigator.prototype.right = function() {
-    if (!this.selected) {
-      this.element.children().eq(0).addClass(this.options.selected);
+    if (!this.$selected) {
+      this.select(this.$element.children().eq(0));
+    } else {
+      var left = this.$selected.position().left + this.$selected.width();
+      var top = this.$selected.position().top;
+
+      var next = this.elementsAfter(left, 0).reduce(function(prev, curr) {
+        curr = $(curr);
+        var currDistance = curr.position();
+        currDistance = Math.abs(currDistance.left - left) + Math.abs(currDistance.top - top);
+        if (currDistance < prev.distance) {
+          return {
+            distance: currDistance,
+            element: curr
+          };
+        }
+        return prev;
+      }, {
+        distance: Infinity
+      });
+
+      this.select(next.element);
     }
   };
 
   Navigator.prototype.down = function() {
-    if (!this.selected) {
-      this.element.children().eq(0).addClass(this.options.selected);
+    if (!this.$selected) {
+      this.select(this.$element.children().eq(0));
+    } else {
+      var left = this.$selected.position().left;
+      var top = this.$selected.position().top + this.$selected.height();
+
+      var next = this.elementsAfter(0, top).reduce(function(prev, curr) {
+        curr = $(curr);
+        var currDistance = curr.position();
+        currDistance = Math.abs(currDistance.left - left) + Math.abs(currDistance.top - top);
+        if (currDistance < prev.distance) {
+          return {
+            distance: currDistance,
+            element: curr
+          };
+        }
+        return prev;
+      }, {
+        distance: Infinity
+      });
+
+      this.select(next.element);
     }
   };
 
   Navigator.prototype.selected = function() {
-    return this.selected;
+    return this.$selected;
+  };
+
+  Navigator.prototype.select = function(element) {
+    element = $(element);
+    if (element.length && element !== this.$selected) {
+      // Unselect previous element.
+      if (this.$selected) {
+        this.$selected.removeClass(this.$options.selected);
+      }
+      // Select given element.
+      element.addClass(this.$options.selected);
+      this.$selected = element;
+    }
+  };
+
+  Navigator.prototype.elementsAfter = function(left, top) {
+    return $.grep(this.$element.children(), function(el) {
+      var pos = $(el).position();
+      return pos.left >= left && pos.top >= top;
+    });
+  };
+
+  Navigator.prototype.elementsBefore = function(left, top) {
+    return $.grep(this.$element.children(), function(el) {
+      var pos = $(el).position();
+      return pos.left <= left && pos.top <= top;
+    });
   };
 
   //--------------------------//
@@ -108,8 +215,9 @@
 
     // Parse arguments.
     var args = Array.prototype.slice.call(arguments, 1);
+    var retval;
 
-    return this.each(function() {
+    this.each(function() {
 
       var $this = $(this),
         instance = $this.data('navigator');
@@ -121,10 +229,16 @@
 
       // Invoke given method with given arguments.
       if (typeof method === 'string' && instance[method]) {
-        instance[method].apply(instance, args);
+        retval = instance[method].apply(instance, args);
       }
 
     });
+
+    if (retval === undefined) {
+      retval = this;
+    }
+
+    return retval;
   };
 
   $.fn.navigator.Constructor = Navigator;
