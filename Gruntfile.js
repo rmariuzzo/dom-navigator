aabd18b5d816f50c09c9cda583328d81680615ec
Rubens Mariuzzo <rubens@mariuzzo.com>
2016-07-31 15:18:29 -0400

    Updated jshint's Grunt plugin to support esversion: 6.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,130 +1,132 @@
-'use strict';
+(function () {
+    'use strict';
 
-module.exports = (grunt) => {
+    module.exports = (grunt) => {
 
-    // Load all grunt tasks
-    require('load-grunt-tasks')(grunt);
+        // Load all grunt tasks
+        require('load-grunt-tasks')(grunt);
 
-    // Show elapsed time at the end
-    require('time-grunt')(grunt);
+        // Show elapsed time at the end
+        require('time-grunt')(grunt);
 
-    // Project configuration.
-    grunt.initConfig({
+        // Project configuration.
+        grunt.initConfig({
 
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
+            // Metadata.
+            pkg: grunt.file.readJSON('package.json'),
 
-        banner: `/*!
-         * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>
-         *
-         * <%= pkg.homepage %>
-         * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> Licensed MIT
-         */
-         `,
+            banner: `/*!
+ * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>
+ *
+ * <%= pkg.homepage %>
+ * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> Licensed MIT
+ */
+`,
 
-        filename: 'dom-navigator',
+            filename: 'dom-navigator',
 
-        // Task configuration.
-        clean: {
-            files: ['dist']
-        },
-
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
+            // Task configuration.
+            clean: {
+                files: ['dist']
             },
-            dist: {
-                src: ['src/<%= filename %>.js'],
-                dest: 'dist/<%= filename %>.js'
-            }
-        },
 
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/<%= filename %>.min.js'
-            }
-        },
-
-        qunit: {
-            all: {
+            concat: {
                 options: {
-                    urls: ['http://0.0.0.0:9000/test/main.html']
+                    banner: '<%= banner %>',
+                    stripBanners: true
+                },
+                dist: {
+                    src: ['src/<%= filename %>.js'],
+                    dest: 'dist/<%= filename %>.js'
                 }
-            }
-        },
+            },
 
-        connect: {
-            server: {
+            uglify: {
                 options: {
-                    hostname: '*',
-                    port: 9000
+                    banner: '<%= banner %>'
+                },
+                dist: {
+                    src: '<%= concat.dist.dest %>',
+                    dest: 'dist/<%= filename %>.min.js'
+                }
+            },
+
+            qunit: {
+                all: {
+                    options: {
+                        urls: ['http://0.0.0.0:9000/test/main.html']
+                    }
+                }
+            },
+
+            connect: {
+                server: {
+                    options: {
+                        hostname: '*',
+                        port: 9000
+                    }
+                }
+            },
+
+            jshint: {
+                options: {
+                    jshintrc: true,
+                    reporter: require('jshint-stylish')
+                },
+                gruntfile: ['Gruntfile.js'],
+                src: ['src/**/*.js'],
+                test: ['test/**/*.js']
+            },
+
+            watch: {
+                gruntfile: {
+                    files: '<%= jshint.gruntfile.src %>',
+                    tasks: ['jshint:gruntfile']
+                },
+                src: {
+                    files: '<%= jshint.src.src %>',
+                    tasks: ['jshint:src', 'qunit']
+                },
+                test: {
+                    files: '<%= jshint.test.src %>',
+                    tasks: ['jshint:test', 'qunit']
+                }
+            },
+
+            babel: {
+                options: {
+                    sourceMap: false,
+                    presets: ['es2015']
+                },
+                dist: {
+                    files: {
+                        'dist/jquery.checkboxes-<%= pkg.version %>.js': 'src/jquery.checkboxes.js'
+                    }
                 }
             }
-        },
+        });
 
-        jshint: {
-            options: {
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'Gruntfiles.js',
-                'src/**/*.js',
-                'test/**/*.js'
-            ]
-        },
+        // Default task.
+        grunt.registerTask('default', [
+            'jshint',
+            'connect',
+            'qunit',
+            'clean',
+            'concat',
+            'uglify'
+        ]);
 
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            src: {
-                files: '<%= jshint.src.src %>',
-                tasks: ['jshint:src', 'qunit']
-            },
-            test: {
-                files: '<%= jshint.test.src %>',
-                tasks: ['jshint:test', 'qunit']
-            }
-        },
+        grunt.registerTask('serve', [
+            'connect',
+            'watch'
+        ]);
 
-        babel: {
-            options: {
-                sourceMap: false,
-                presets: ['es2015']
-            },
-            dist: {
-                files: {
-                    'dist/jquery.checkboxes-<%= pkg.version %>.js': 'src/jquery.checkboxes.js'
-                }
-            }
-        }
-    });
+        grunt.registerTask('test', [
+            'jshint',
+            'connect',
+            'qunit'
+        ]);
 
-    // Default task.
-    grunt.registerTask('default', [
-        'jshint',
-        'connect',
-        'qunit',
-        'clean',
-        'concat',
-        'uglify'
-    ]);
+    };
 
-    grunt.registerTask('serve', [
-        'connect',
-        'watch'
-    ]);
-
-    grunt.registerTask('test', [
-        'jshint',
-        'connect',
-        'qunit'
-    ]);
-
-};
+})();
